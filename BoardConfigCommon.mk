783144e6028c094b51cde06714d9783a92e750d0
GitFASTBOOT <ahmedshahinx0x@gmail.com>
2025-03-08 02:57:01 +0200

    mt6768-common: define kernel flags

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -5,6 +5,7 @@
 #
 
 COMMON_PATH := device/motorola/mt6768-common
+KERNEL_PATH := device/motorola/mt6768-kernel
 
 # Architecture
 TARGET_ARCH := arm64
@@ -19,6 +20,34 @@ TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := generic
 TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a55
+
+# Kernel
+BOARD_BOOT_HEADER_VERSION := 4
+BOARD_KERNEL_BASE := 0x40078000
+BOARD_KERNEL_OFFSET := 0x00008000
+BOARD_KERNEL_PAGESIZE := 0x00001000
+BOARD_TAGS_OFFSET := 0x0bc08000
+BOARD_RAMDISK_OFFSET := 0x07c08000
+BOARD_KERNEL_CMDLINE += bootopt=64S3,32N2,64N2
+
+BOARD_RAMDISK_USE_LZ4 := true
+BOARD_INCLUDE_DTB_IN_BOOTIMG := true
+BOARD_KERNEL_IMAGE_NAME := Image.gz
+BOARD_USES_GENERIC_KERNEL_IMAGE := true
+
+BOARD_MKBOOTIMG_ARGS += \
+    --dtb_offset $(BOARD_TAGS_OFFSET) \
+    --header_version $(BOARD_BOOT_HEADER_VERSION) \
+    --kernel_offset $(BOARD_KERNEL_OFFSET) \
+    --ramdisk_offset $(BOARD_RAMDISK_OFFSET) \
+    --tags_offset $(BOARD_TAGS_OFFSET)
+
+TARGET_NO_KERNEL_OVERRIDE := true
+TARGET_KERNEL_SOURCE := device/motorola/mt6768-kernel/kernel-headers
+
+LOCAL_KERNEL := $(KERNEL_PATH)/$(BOARD_KERNEL_IMAGE_NAME)
+PRODUCT_COPY_FILES += \
+	$(LOCAL_KERNEL):kernel
 
 # Inherit the proprietary files
 include vendor/motorola/mt6768-common/BoardConfigVendor.mk
