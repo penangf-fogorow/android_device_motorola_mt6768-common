f7e41bb2657f73316e083af2d4cd5c41edeb2c6b
GitFASTBOOT <ahmedshahinx0x@gmail.com>
2025-03-08 02:41:47 +0200

    mt6768-common: initial common tree

---
+++ BoardConfigCommon.mk
@@ -0,0 +1,10 @@
+#
+# Copyright (C) 2025 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+COMMON_PATH := device/motorola/mt6768-common
+
+# Inherit the proprietary files
+include vendor/motorola/mt6768-common/BoardConfigVendor.mk
---
+++ device.mk
@@ -0,0 +1,8 @@
+#
+# Copyright (C) 2025 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit the proprietary files
+$(call inherit-product, vendor/motorola/mt6768-common/mt6768-common-vendor.mk)
